ace79c81b78b94647af62af44f7b0f59df8db317
lilinchandra <lilinchandra@gmail.com>
2016-09-25 19:09:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/article-one', function (req, res){
+    res.send('Article one has been requested');
+    
 });
 
 app.get('/article-two', function (req,res){
