d79a1325fb03ab7dd41977d18be01d754a4df353
lilinchandra <lilinchandra@gmail.com>
2016-10-12 22:00:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/ui/Pandora.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Pandora.png'));
 });
 
+app.get('/ui/abb.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'abb.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
